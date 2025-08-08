f9543a5d7a1ec3899003cc7f6d21d31c637d70a4
4614s <onurgral1446.2@gmail.com>
2025-08-08 15:27:23 +0300

    Create nav.js

---
+++ docs/nav.js
@@ -0,0 +1,12 @@
+if(location.hostname.includes("ezoradom.js.org")){
+  document.addEventListener("DOMContentLoaded",()=>{
+     let nav = document.querySelector('nav');
+     nav.innerText = '☰';
+     nav.style.fontSize = '2rem';
+     nav.style.position = 'fixed';
+     nav.style.background = '#1B0E3D';
+     nav.style.paddingInline = '10px';
+     nav.style.borderRadius = '10px';
+     nav.onclick = () => location.href='/docs/';
+    });
+}
